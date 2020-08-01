1897dd54a70da2ee3a374b7d697f4629fd574e57
hfurkanvural <hfurkanvural@gmail.com>
2020-08-01 23:17:57 +0300

    repositories created

---
+++ spring-datajpa-postgresql/src/main/java/com/hfv/repo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.hfv.repo;
+
+import com.hfv.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
