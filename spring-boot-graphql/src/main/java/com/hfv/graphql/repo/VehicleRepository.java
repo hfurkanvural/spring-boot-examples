46cf34bbfda2aff4f4dd438dd957820bb6df9d78
hfurkanvural <hfurkanvural@gmail.com>
2020-08-21 23:33:04 +0300

    Creating simple class and table for operations

---
+++ spring-boot-graphql/src/main/java/com/hfv/graphql/repo/VehicleRepository.java
@@ -0,0 +1,12 @@
+package com.hfv.graphql.repo;
+
+import com.hfv.graphql.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    List<Vehicle> getByTypeLike(String type);
+
+    List<Vehicle> getByModelCodeOrBrandName(String type);
+}
